dc8c49beddd854d767f6e2c79ae0a05261a449a9
VenkatesanParamasivam <venkynov10@gmail.com>
2022-01-16 17:23:56 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 
 def app():
-    absFilePath = os.path.abspath(__file__)                # Absolute Path of the module
+    absFilePath = os.path.dirname(fileDir)                # Absolute Path of the module
     st.write(absFilePath)
     pweb = """<a href='https://www.youtube.com/channel/UCespXFy72CY6ok9l4V6WJcw' target="_blank">https://www.youtube.com/channel/UCespXFy72CY6ok9l4V6WJcw</a>"""
     sm_li = """<a href='https://www.linkedin.com/in/andymcdonaldgeo/' target="_blank"><img src='https://cdn.exclaimer.com/Handbook%20Images/linkedin-icon_32x32.png'></a>"""
