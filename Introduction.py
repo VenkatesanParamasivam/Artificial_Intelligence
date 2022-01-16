5a7c4446f4dd30804066f80ac80a020aed0ae0c8
VenkatesanParamasivam <venkynov10@gmail.com>
2022-01-16 17:17:00 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -36,7 +36,7 @@ Quick learner, Flexible, Ability to understand concepts quickly, Eager to learn 
              The Artificial Intelligence tutorial provides an introduction to AI which will help you to understand the concepts behind Artificial Intelligence. In this tutorial, we have also discussed various popular topics such as History of AI, applications of AI, deep learning, machine learning, natural language processing, Reinforcement learning, Q-learning, Intelligent agents, Various search algorithms, etc.
 
 Our AI tutorial is prepared from an elementary level so you can easily understand the complete tutorial from basic concepts to the high-level concepts.""")
-    image = Image.open(r'D:\Website\photos\AI.jpg')
+    image = Image.open(r'Images/AI.jpg')
 
     st.image(image, caption='What is AI')
     
