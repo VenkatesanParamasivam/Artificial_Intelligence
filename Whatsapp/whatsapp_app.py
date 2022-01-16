7737c79ad4bf5e5ab2bd022c357bde910885f264
VenkatesanParamasivam <venkynov10@gmail.com>
2022-01-16 18:45:50 +0530

    Update whatsapp_app.py

--- Whatsapp/whatsapp_app.py
+++ Whatsapp/whatsapp_app.py
@@ -368,7 +368,7 @@ def app():
     Pandas Dataframe items
     """
     # Parsing YAML file
-    config = r'D:/Projects/Streamlit_webapp/Whatsapp/configs/app_configuration.yml'
+    config = .Whatsapp/configs/app_configuration.yml'
     config = yaml.safe_load(open(config))
     # configure logging
     log_config = config['logging']
@@ -391,7 +391,7 @@ def app():
     
     # Static File processin For DEMO purpose
     if c2.button("Try Demo"):
-        with open(r'D:/Projects/Streamlit_webapp/Whatsapp/configs/demo_chat.txt', 'r',encoding="utf8") as read_file:
+        with open(./Whatsapp/configs/demo_chat.txt', 'r',encoding="utf8") as read_file:
             data = read_file.read()
         file_process(data, config)
     
